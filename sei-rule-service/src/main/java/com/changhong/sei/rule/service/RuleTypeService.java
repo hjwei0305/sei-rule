833a91e5a2bcb8ece7004c7074ccf3848328729c
王锦光 <wangjg@changhong.com>
2021-01-14 09:40:19 +0800

    完善规则类型(RuleType)业务逻辑

--- sei-rule-service/src/main/java/com/changhong/sei/rule/service/RuleTypeService.java
+++ sei-rule-service/src/main/java/com/changhong/sei/rule/service/RuleTypeService.java
@@ -2,6 +2,8 @@ package com.changhong.sei.rule.service;
 
 import com.changhong.sei.core.dao.BaseEntityDao;
 import com.changhong.sei.core.service.BaseEntityService;
+import com.changhong.sei.core.service.bo.OperateResult;
+import com.changhong.sei.rule.dao.RuleTreeNodeDao;
 import com.changhong.sei.rule.dao.RuleTypeDao;
 import com.changhong.sei.rule.entity.RuleType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +20,26 @@ import org.springframework.stereotype.Service;
 public class RuleTypeService extends BaseEntityService<RuleType> {
     @Autowired
     private RuleTypeDao dao;
+    @Autowired
+    private RuleTreeNodeDao ruleTreeNodeDao;
 
     @Override
     protected BaseEntityDao<RuleType> getDao() {
         return dao;
     }
 
+    /**
+     * 删除数据保存数据之前额外操作回调方法 子类根据需要覆写添加逻辑即可
+     *
+     * @param id 规则类型Id
+     */
+    @Override
+    protected OperateResult preDelete(String id) {
+        // 检查规则类型是否配置了规则树
+        if (ruleTreeNodeDao.isExistsByProperty("ruleTypeId", id)) {
+            // 规则类型已经配置了规则树，禁止删除！
+            return OperateResult.operationFailure("00014");
+        }
+        return super.preDelete(id);
+    }
 }
